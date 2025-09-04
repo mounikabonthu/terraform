d1f5c6a2672c24e65ad1b623738de4eb235903b6
mounikabonthu <bontumonika@gmail.com>
2025-09-04 16:32:24 +0530

    terraform ec2

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,3 +1,14 @@
+resource "aws_instance" "terraform-demo" {
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+
+  tags = {
+    Name    = "terraform-demo"
+    Purpose = "terraform-practice"
+  }
+}
+
 resource "aws_security_group" "allow_ssh_terraform" {
   name        = "allow_ssh"
   description = "Allow port no 22 for SSH access"
@@ -20,16 +31,5 @@ resource "aws_security_group" "allow_ssh_terraform" {
 
   tags = {
     Name = "allow_ssh"
-  }
-}
-
-resource "aws_instance" "terraform-demo" {
-  ami                    = "ami-09c813fb71547fc4f"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
-
-  tags = {
-    Name    = "terraform-demo"
-    Purpose = "terraform-practice"
   }
 }
