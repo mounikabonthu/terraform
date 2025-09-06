019e4ddb227dfc3b670a27bb4d79b2aae33523d5
mounikabonthu <bontumonika@gmail.com>
2025-09-06 11:20:53 +0530

    locals

---
+++ locals/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "terraform-demo" {
+  ami                    = local.ami_id
+  instance_type          = local.instance_type
+  vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+
+  tags = {
+    Name    = local.name
+    Purpose = "terraform-practice"
+  }
+}
+
+resource "aws_security_group" "allow_ssh_terraform" {
+  name        = "allow_ssh"
+  description = "Allow port no 22 for SSH access"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
---
+++ locals/variables.tf
@@ -0,0 +1,12 @@
+
+variable "project" {
+    default = "expense"
+}
+
+variable "component" {
+    default = "backend"
+}
+
+variable "environment" {
+    default = "dev"
+}
